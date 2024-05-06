5f30bf2f92a300364b3bb49bf57e83b93a67c8dc
Faheem-Siddiqi <faheem_siddiqi@yahoo.com>
2024-05-06 18:01:25 +0500

    d

--- client/src/components/UserDashboard.tsx
+++ client/src/components/UserDashboard.tsx
@@ -9,7 +9,6 @@ const UserDashboard: React.FC = () => {
   const [userOption, setUserOption] = useState("buy");
   const [period, setPeriod] = useState("Newest");
   const togglePeriod = () => {
-
     setPeriod((prevPeriod) => (prevPeriod === "Newest" ? "Oldest" : "Newest"));
   };
   const handleOptionChange = (e: any) => {
@@ -40,113 +39,104 @@ const UserDashboard: React.FC = () => {
             </div>
           </div>
         </div>
-
-
-        
         {/*  */}
-
-
-
         <div className="relative overflow-x-auto rounded-lg bdr bgLightGret mr-3 ml-3  lg:ml-4 lg:mr-8 my-5 p-4">
-            <p className="text-xl  ">Tabular Analysis </p>
-            <p className="mt-3">
-              Tabular Analysis of <span>{userOption}</span> requests in{" "}
-              {period === "Newest"
-                ? "newest to oldest order"
-                : "oldest to newest order"}{" "}
-            </p>
-            <div className="flex my-2 gap-2 items-center absolute right-5 simpleButton1 w-fit">
-              <FaFilter />
-              <select
-                className="bg-transparent outline-none w-full"
-                name="requestType"
-                id="requestType"
-                onChange={handleOptionChange}
-              >
-                <option className=" text-primaryColor" value="buy">
-                  Buy Requests
-                </option>
-                <option className=" text-primaryColor" value="sell">
-                  Sell Requests
-                </option>
-              </select>
-            </div>
-            <br />
-            <table
-              id="table"
-              className="w-full whitespace-nowrap !text-center my-5"
+          <p className="text-xl  ">Tabular Analysis </p>
+          <p className="mt-3">
+            Tabular Analysis of <span>{userOption}</span> requests in{" "}
+            {period === "Newest"
+              ? "newest to oldest order"
+              : "oldest to newest order"}{" "}
+          </p>
+          <div className="flex my-2 gap-2 items-center absolute right-5 simpleButton1 w-fit">
+            <FaFilter />
+            <select
+              className="bg-transparent outline-none w-full"
+              name="requestType"
+              id="requestType"
+              onChange={handleOptionChange}
             >
-              <thead>
-                <tr className="border-b border-primaryColor text-left">
-                  <th className="px-6 py-3 text-md font-bold text-primaryColor uppercase tracking-wider">
-                    ID
-                  </th>
-                  <th className="px-6 py-3  text-md font-bold text-primaryColor uppercase tracking-wider">
-                    Date
-                  </th>
-                  <th className="px-6 py-3  text-md font-bold text-primaryColor uppercase tracking-wider">
-                    ZK Tokens
-                  </th>
-                  <th className="px-6 py-3  text-md font-bold text-primaryColor uppercase tracking-wider">
-                    User Name
-                  </th>
-                  <th className="px-6 py-3  text-md font-bold text-primaryColor uppercase tracking-wider">
-                    Status
-                  </th>
-                  <CgArrowsExchangeAltV
-                    className="text-3xl mt-[0.71rem] cursor-pointer hover:text-primaryColor duration-300"
-                    onClick={togglePeriod}
-                  />
-                </tr>
-              </thead>
-              <tbody className="border-t"></tbody>
-              {/* {sortedTokens.map((token: any, index) => ( */}
-                <tr
-                  // key={index}
-                  className="text-left"
-                  style={{ borderBottom: "1px solid #41464580" }}
-                >
-                  <td className="px-6 py-3 textBasic">
-                    {/* {token._id} */}
-                    
-                    </td>
-                  <td className="px-6 py-3 textBasic">
-                    {/* {new Date(
+              <option className=" text-primaryColor" value="buy">
+                Buy Requests
+              </option>
+              <option className=" text-primaryColor" value="sell">
+                Sell Requests
+              </option>
+            </select>
+          </div>
+          <br />
+          <table
+            id="table"
+            className="w-full whitespace-nowrap !text-center my-5"
+          >
+            <thead>
+              <tr className="border-b border-primaryColor text-left">
+                <th className="px-6 py-3 text-md font-bold text-primaryColor uppercase tracking-wider">
+                  ID
+                </th>
+                <th className="px-6 py-3  text-md font-bold text-primaryColor uppercase tracking-wider">
+                  Date
+                </th>
+                <th className="px-6 py-3  text-md font-bold text-primaryColor uppercase tracking-wider">
+                  ZK Tokens
+                </th>
+                <th className="px-6 py-3  text-md font-bold text-primaryColor uppercase tracking-wider">
+                  User Name
+                </th>
+                <th className="px-6 py-3  text-md font-bold text-primaryColor uppercase tracking-wider">
+                  Status
+                </th>
+                <CgArrowsExchangeAltV
+                  className="text-3xl mt-[0.71rem] cursor-pointer hover:text-primaryColor duration-300"
+                  onClick={togglePeriod}
+                />
+              </tr>
+            </thead>
+            <tbody className="border-t"></tbody>
+            {/* {sortedTokens.map((token: any, index) => ( */}
+            <tr
+              // key={index}
+              className="text-left"
+              style={{ borderBottom: "1px solid #41464580" }}
+            >
+              <td className="px-6 py-3 textBasic">
+                {/* {token._id} */}
+              </td>
+              <td className="px-6 py-3 textBasic">
+                {/* {new Date(
                       token.dateTimeField || token.SellTokendateTimeField
                     ).toLocaleString()} */}
-                    Date
-                  </td>
-                  <td className="px-6 py-3 textBasic">
-                    Token Amount
-                    {/* {token.Tokens || token.TokensAmount} */}
-                  </td>
-                  <td className="px-6 py-3 textBasic">
-                    Admin Name
-                    {/* {token.buyer?.username || token.seller?.username} */}
-                  </td>
-                  <td
-                    // className={`px-6 py-3 textBasic  ${token.transactionStatus === "Declined"
-                    //   ? "!text-red-400"
-                    //   : ""
-                    //   } ${token.transactionStatus === "Pending"
-                    //     ? "!text-yellow-400"
-                    //     : ""
-                    //   } ${token.transactionStatus === "Approved"
-                    //     ? "!text-green-400"
-                    //     : ""
-                    //   }`}
-
-                  >
-                   Class ko uncomment krna ha  status color class ki base pr change horha
-                   
-                    {/* {token.transactionStatus || token.status} */}
-                  </td>
-                </tr>
+                Date
+              </td>
+              <td className="px-6 py-3 textBasic">
+                Token Amount
+                {/* {token.Tokens || token.TokensAmount} */}
+              </td>
+              <td className="px-6 py-3 textBasic">
+                Admin Name
+                {/* {token.buyer?.username || token.seller?.username} */}
+              </td>
+              <td
+              // className={`px-6 py-3 textBasic  ${token.transactionStatus === "Declined"
+              //   ? "!text-red-400"
+              //   : ""
+              //   } ${token.transactionStatus === "Pending"
+              //     ? "!text-yellow-400"
+              //     : ""
+              //   } ${token.transactionStatus === "Approved"
+              //     ? "!text-green-400"
+              //     : ""
+              //   }`}
+              >
+                Class ko uncomment krna ha  status color class ki base pr change horha
+                {/* {token.transactionStatus || token.status} */}
+              </td>
+            </tr>
+        
+        
                ))}
-            </table>
-          </div>
-
-
+          </table>
+        </div>
       </div>
     </>
   );
