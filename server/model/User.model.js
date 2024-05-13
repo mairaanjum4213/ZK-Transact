ac1c5a026c0b9e3859e5840bae2f663a7a0572fe
mairaanjum4213 <maira_anjum001@yahoo.com>
2024-05-14 00:27:28 +0500

    .

--- server/model/User.model.js
+++ server/model/User.model.js
@@ -1,62 +1,71 @@
-import mongoose , {Schema} from "mongoose";
-export const UserSchema = new mongoose.Schema({
+import mongoose, { Schema } from "mongoose";
+export const UserSchema = new mongoose.Schema(
+  {
     username: {
-        type: String,
-        required: [true, "Please provide unique Username"],
-        unique: [true, "Username Exist"]
+      type: String,
+      required: [true, "Please provide unique Username"],
+      unique: [true, "Username Exist"],
     },
     password: {
-        type: String,
-        required: [true, "Please provide a password"],
-        unique: false,
+      type: String,
+      required: [true, "Please provide a password"],
+      unique: false,
     },
     email: {
-        type: String,
-        required: [true, "Please provide a unique email"],
-        unique: true,
+      type: String,
+      required: [true, "Please provide a unique email"],
+      unique: true,
     },
     fullName: { type: String },
     mobile: { type: Number },
     address: { type: String },
     kycStatus: {
-        type: Boolean,
-        default: true
+      type: Boolean,
+      default: false,
     },
     region: {
-        type: String,
-        required: [true, "Please provide a region"],
+      type: String,
+      required: [true, "Please provide a region"],
     },
     gender: {
-        type: String,
-        default: "male",
-        enum: ["male", "female"]
+      type: String,
+      default: "male",
+      enum: ["male", "female"],
     },
     profilePic: {
-        type: String,
-        default: "pic",
+      type: String,
+      default: "pic",
     },
     isMerchant: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false,
+    },
+    isRepresentative: {
+      type: Boolean,
+      default: false,
     },
     merchantFee: {
-        type: Number,
-        default: 0
-    },  
-    accounts: [{
+      type: Number,
+      default: 0,
+    },
+    accounts: [
+      {
         type: Schema.Types.ObjectId,
-        ref: 'Account'
-    }],
+        ref: "Account",
+      },
+    ],
     allowServices: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false,
     },
     wallet: {
-        type: Schema.Types.ObjectId,
-        ref: 'Wallet',
-        default: null
-    }
-},{timestamps:true});
+      type: Schema.Types.ObjectId,
+      ref: "Wallet",
+      default: null,
+    },
+  },
+  { timestamps: true }
+);
 
 //if already have user model in mongodb database then use exisiting model otherwise return new one
-export default mongoose.models.Users || mongoose.model('User', UserSchema);
+export default mongoose.models.Users || mongoose.model("User", UserSchema);
